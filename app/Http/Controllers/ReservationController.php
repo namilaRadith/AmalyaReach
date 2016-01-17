b40ea6ad0db5c6a7194c973aa11418348bdba23e
Sameera Chandimal <sameerachandrasena@gmail.com>
2016-01-17 19:43:43 +0530

    reservation controller added noww222

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,16 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+
+class ReservationController extends Controller {
+
+    public function checkAvailableRoomsForm2(){
+        return view('pages.client.reservationForm2');
+    }
+
+ //    adsfdsfdsf ds
+}
